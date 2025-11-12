e169b29dfbeb248ddd3664a00ee121127691726b
TAN0N <at3280@student.uni-lj.si>
2025-11-12 07:22:05 +0100

    Restructured project

---
+++ Express/Graphics/AnimatedSprite.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Express.Graphics;
+
+public class AnimatedSprite
+{
+    protected List<AnimatedSpriteFrame> _frames;
+    protected double _duration;
+    protected bool _looping;
+
+    public AnimatedSprite(double duration)
+    {
+        _frames = new List<AnimatedSpriteFrame>();
+        _duration = duration;
+    }
+
+    public double Duration {
+        get => _duration;
+        set => _duration = value;
+    }
+
+    public bool Looping { 
+        get => _looping;
+        set => _looping = value;
+    }
+
+    void SetLoopingDuration(double duration)
+    { 
+        _looping = true;
+        _duration = duration;
+    }
+    public void AddFrame(AnimatedSpriteFrame frame) { 
+        _frames.Add(frame);
+        _frames.Sort((x, y) => x.Start.CompareTo(y.Start));
+    }
+    public Sprite SpriteAtTime(double time)
+    {
+        if (_looping)
+        {
+            int loops = (int)System.Math.Floor(time / _duration);
+            time -= loops * _duration;
+        }
+        if (time >= Duration)
+            return null;
+        for (int i = 0; i < _frames.Count - 1; i++)
+        { 
+            AnimatedSpriteFrame nextFrame = _frames[i+1];
+            if (nextFrame.Start > time)
+            {
+                AnimatedSpriteFrame frame = _frames[i];
+                return frame.Sprite;
+            }
+        }
+        AnimatedSpriteFrame frameDefault = _frames[^1];
+        return frameDefault.Sprite;
+    }
+}
---
+++ Express/Graphics/Sprite.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Express.Graphics;
+
+public class Sprite
+{
+    private Texture2D _texture;
+    private Rectangle _sourceRectangle;
+    private Vector2 _origin;
+
+    public Texture2D Texture
+    { 
+        get { return _texture; }
+        set { _texture = value; }
+    }
+
+    public Rectangle SourceRectangle
+    {
+        get { return _sourceRectangle; }
+        set { _sourceRectangle = value; }
+    }
+
+    public Vector2 Origin
+    {
+        get { return _origin; }
+        set { _origin = value; }
+    } 
+}
