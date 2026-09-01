0171c7e68ced8008f8dab5b56db041957ca1fe00
TAN0N <at3280@student.uni-lj.si>
2026-09-01 12:35:06 +0200

    Fixed textures

--- Express/obj/Debug/net8.0/Express.AssemblyInfo.cs
+++ Express/obj/Debug/net8.0/Express.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Express")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+292aa81729eb746499dcf3ad2deaae1fdacf94c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f90f7488716957513aaceced5f57b8a52ba5def4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Express")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Express")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
